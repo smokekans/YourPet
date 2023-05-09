2050af06f23a6bb92ffbd5fea85f5be6249b2815
Ibragimov-96 <dima.ibragimov.1996@ukr.net>
2023-05-09 11:58:58 +0300

    поля и животные заполняются данными с бєка

--- src/components/User/UserData/UserData.jsx
+++ src/components/User/UserData/UserData.jsx
@@ -1,18 +1,19 @@
 
-// import {getUser} from '../../../redux/user/userOperations'
+
 import { useSelector } from 'react-redux'
-// import {getNameUser} from '../../../redux/user/userSelectors'
-import {getUser} from '../../../redux/auth/authSelectors'
+import {getNameUser} from '../../../redux/user/userSelectors'
+
 import UserLogOut from '../UserLogOut/UserLogOut'
 import UserDataItem from '../UserDataItem/UserDataItem'
 
 function UserData() {
  
  
-//  const user =  getUser()
- const user = useSelector(getUser)
- const {image }= user
-console.log(user)
+
+ const user = useSelector(getNameUser)
+ console.log(user)
+ const {image }= user.user
+
   return <><div>
    <h2>My information:</h2> 
 <img src={image} alt='' height='182' width='182'  /><br/>
