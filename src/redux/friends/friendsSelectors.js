8b169f77883a74644d85638450b8e831de5679ae
SoloviovO <soloviov.o.m@gmail.com>
2023-05-10 16:16:07 +0300

    add sponsor page

--- src/redux/friends/friendsSelectors.js
+++ src/redux/friends/friendsSelectors.js
@@ -1,1 +1,2 @@
-export const getNameCompany = state => state.friends.nameCompany;
+export const selectFriends = state => state.friends.friends;
+export const selectIsisLoaded = state => state.friends.isLoaded;
