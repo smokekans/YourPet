e225b3e64f7a0814da1f97a7941b2c1b22b75ec7
Maksym-Kovorotnyi <Kovorotny1@gmail.com>
2023-05-11 20:33:24 +0300

    Update NoticesPagination.jsx

--- src/components/Notices/NoticesPagination/NoticesPagination.jsx
+++ src/components/Notices/NoticesPagination/NoticesPagination.jsx
@@ -1,24 +1,29 @@
 import React, { useState } from 'react';
 import { PaginationControl } from 'react-bootstrap-pagination-control';
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
+import { useParams } from 'react-router-dom/dist';
 import { getNotices } from 'redux/notices/noticesSelectors';
 
 export const NoticesPagination = () => {
   const noticesItems = useSelector(getNotices).notices;
   const [page, setPage] = useState(1);
+  const dispatch = useDispatch();
+  const { categoryName } = useParams();
+
+  const handleChangePag = e => {
+    console.dir(e.target);
+  };
 
   return (
-    <div>
+    <div onClick={handleChangePag}>
       <PaginationControl
         page={page}
         between={4}
         total={250}
-        limit={20}
+        limit={10}
         changePage={page => {
           setPage(page);
-          console.log(page);
         }}
-        ellipsis={1}
       />
     </div>
   );
