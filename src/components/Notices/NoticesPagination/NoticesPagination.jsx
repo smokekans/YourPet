eb17d4d9994826e90ee67638a223c63fdc3e4aa8
Maksym-Kovorotnyi <Kovorotny1@gmail.com>
2023-05-14 09:25:14 +0300

    Update NoticesPagination.jsx

--- src/components/Notices/NoticesPagination/NoticesPagination.jsx
+++ src/components/Notices/NoticesPagination/NoticesPagination.jsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import { PaginationControl } from 'react-bootstrap-pagination-control';
 import { useDispatch, useSelector } from 'react-redux';
 import { useParams } from 'react-router-dom/dist';
+import { getNoticeByCategory } from 'redux/notices/noticesOperation';
 import { getNotices } from 'redux/notices/noticesSelectors';
 
 export const NoticesPagination = () => {
@@ -10,21 +11,30 @@ export const NoticesPagination = () => {
   const dispatch = useDispatch();
   const { categoryName } = useParams();
 
-  const handleChangePag = e => {
-    console.dir(e.target);
+  const numberOfPages = noticesItems.length / 10;
+
+  const handleChangePag = async e => {
+    const numberLink = e.target.localName;
+    if (numberLink === 'a') {
+      dispatch(getNoticeByCategory({ category: categoryName, page: page + 1 }));
+    }
   };
 
   return (
     <div onClick={handleChangePag}>
-      <PaginationControl
-        page={page}
-        between={4}
-        total={250}
-        limit={10}
-        changePage={page => {
-          setPage(page);
-        }}
-      />
+      {noticesItems.length > 0 ? (
+        <PaginationControl
+          page={page}
+          between={4}
+          total={numberOfPages}
+          limit={10}
+          changePage={page => {
+            setPage(page);
+          }}
+        />
+      ) : (
+        ''
+      )}
     </div>
   );
 };
