76067f5acd4ed871c5c522d1505f8c0dd1731123
Maksym-Kovorotnyi <Kovorotny1@gmail.com>
2023-05-16 17:27:53 +0300

    Update AddToFavoriteButton.jsx

--- src/components/Button/AddToFavoriteButton/AddToFavoriteButton.jsx
+++ src/components/Button/AddToFavoriteButton/AddToFavoriteButton.jsx
@@ -1,26 +1,16 @@
-import { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { toast } from 'react-toastify';
 import { IconButton } from '@mui/material';
 import { ReactComponent as IconHeart } from '../../../images/icons/heart.svg';
-import {
-  addToFavorites,
-  deleteFromFavorite,
-  getCurrentUser,
-} from 'redux/user/userOperations';
-import { getFavorite } from 'redux/user/userSelectors';
+import { addToFavorites, deleteFromFavorite } from 'redux/user/userOperations';
+
 import { getIsLoggedIn } from 'redux/auth/authSelectors';
 import { deleteFavoriteObj } from 'redux/user/userSlice';
 
 const FavoriteIconButton = ({ noticeid }) => {
   const dispatch = useDispatch();
   const isLoggedIn = useSelector(getIsLoggedIn);
-  // const [isFavorites, setIsFavorites] = useState(false);
   const favoriteElement = useSelector(state => state.user.favorite);
-  // const dataArray = Array.isArray(favoriteElement)
-  // ? favoriteElement
-  // : Array.from(favoriteElement);
-  // const isFavorite = dataArray.includes(noticeid);
 
   const handleFavoriteClick = () => {
     if (!isLoggedIn) {
@@ -40,7 +30,7 @@ const FavoriteIconButton = ({ noticeid }) => {
       toast('Added to favorites');
     }
   };
-  console.log(favoriteElement);
+
   return (
     <IconButton
       // color={isFavorites ? 'secondary' : 'default'}
