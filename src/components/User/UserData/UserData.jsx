84a0d1c9b7c236045bed57c42e2896091ff72690
Ibragimov-96 <dima.ibragimov.1996@ukr.net>
2023-05-16 14:37:55 +0300

    изменение иконок при редактировании

--- src/components/User/UserData/UserData.jsx
+++ src/components/User/UserData/UserData.jsx
@@ -4,10 +4,12 @@ import UserLogOut from '../UserLogOut/UserLogOut';
 import UserDataItem from '../UserDataItem/UserDataItem';
 import { updateAvatar } from 'redux/user/userOperations';
 import { getAvatar,isLoading } from 'redux/user/userSelectors';
+import Stack from '@mui/material/Stack';
+import IconButton from '@mui/material/IconButton';
 
 import { Button, Avatar, FilledInput, Container } from '@mui/material';
 import styles from './styles';
-
+import { ReactComponent as PawPrint } from '../../../images/icons/camera.svg';
 function UserData() {
   const dispatch = useDispatch();
   const avatarUser = useSelector(getAvatar);
@@ -41,7 +43,22 @@ function UserData() {
           sx={styles.input}
           onChange={OnSumbit}
         ></FilledInput>
-        <Button onClick={handelAvatar}>Button</Button>
+        <Stack direction="row" alignItems="center" spacing={2}>
+        <IconButton color="primary" aria-label="upload picture" component="label" onClick={handelAvatar}>
+        
+        <PawPrint
+                    className="name"
+                    
+                    edge="end"
+                  ></PawPrint>
+      </IconButton>
+      <Button variant="contained" component="label" onClick={handelAvatar}>
+      Edit photo
+        <input hidden accept="image/*" multiple type="file" />
+      </Button>
+     
+    </Stack>
+        {/* <Button onClick={handelAvatar}>Button</Button> */}
         {Loading?<div></div>:<UserDataItem />}
         
       </div>
